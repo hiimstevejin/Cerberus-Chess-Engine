6124f56573910b6e35337fade452cbaa0346dafc
hiimstevejin <jingle5695@gmail.com>
2026-02-24 02:37:15 -0500

    main and executable command

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.uci import UCI
+
+def main():
+    uci = UCI()
+    uci.uci_loop()
+
+if __name__ == "__main__":
+    main()
---
+++ run_bot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Users/stevejin/Desktop/chess
+/Users/stevejin/.pyenv/shims/uv run --quiet main.py
