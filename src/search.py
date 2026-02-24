73b4425164ab1e8819b79024c3aca747a6c2983f
hiimstevejin <jingle5695@gmail.com>
2026-02-24 02:36:17 -0500

    add iterative deepening and transposition table

--- src/search.py
+++ src/search.py
@@ -1,17 +1,43 @@
 import chess
 from src.evals import evaluate_board
+from src.tt import TT
+import time
 
 # for benchmarking purposes count node computation for search
 class Search:
     def __init__(self):
         # transposition table
-
+        self.tt = TT()
         self.nodes = 0
-        self.stop = False
+        self.stop_flag = False
         self.start_time = 0
         self.time_limit = 0
+        self.killers = [[None]*2 for _ in range(64)]
+        self.history = [[0]*64 for _ in range(64)]
 
-    def negamax(self, board:chess.Board, alpha:int, beta:int, depth:int, ply:int):
+    def start_search(self, board, time_limit=5.0):
+        self.stop_flag = False
+        self.nodes = 0
+        self.start_time = time.time()
+        self.time_limit = time_limit
+
+        best_move = None
+
+        for depth in range(1,100):
+            score, move = self.negamax(board, -float('inf'), float('inf'), depth, 0 )
+            if self.stop_flag:
+                break
+
+            best_move = move
+
+            print(f"info depth {depth} score cp {score} nodes {self.nodes} pv {best_move}")
+
+            if self.check_time():
+              break
+
+        return best_move
+
+    def negamax(self, board:chess.Board, alpha:float, beta:float, depth:int, ply:int):
         """
         Perform Negamax search with alpha-beta pruning.
 
@@ -48,36 +74,88 @@ class Search:
             - Detects checkmate/stalemate if no legal moves exist
             - Uses simple move ordering (captures first) to improve pruning
         """
+        if self.nodes & 2047 == 0:
+            self.check_time()
+
+        if self.stop_flag:
+            return 0, None
+
         self.nodes += 1
+
+        key = hash(board.fen())
+        tt_entry = self.tt.probe(key)
+        tt_move = None
+
+        if tt_entry:
+            tt_move = tt_entry['move']
+            if tt_entry['depth'] >= depth:
+                if tt_entry['flag'] == self.tt.EXACT:
+                    return tt_entry['score'], tt_entry['move']
+                if tt_entry['flag'] == self.tt.LOWERBOUND:
+                    alpha = max(alpha, tt_entry['score'])
+                elif tt_entry['flag'] == self.tt.UPPERBOUND:
+                    beta = min(beta, tt_entry['score'])
+                if alpha >= beta:
+                    return tt_entry['score'], tt_entry['move']
+
+        if depth == 0:
+            return self.quiescence(board, alpha, beta), None
+
         if board.is_game_over():
             if board.is_checkmate():
                 return -100000 + ply, None
             else:
                 return 0, None
 
-        if depth == 0:
-            return self.quiescence(board, alpha, beta), None
+        def score_move(move):
+            if move == tt_move:
+                return 2000000
+
+            if board.is_capture(move):
+                return 1000000
+
+            if move == self.killers[ply][0]:
+                return 90000
+            if move == self.killers[ply][1]:
+                return 800000
+
+            return self.history[move.from_square][move.to_square]
 
         moves = list(board.legal_moves)
-        captures = [m for m in moves if board.is_capture(m)]
-        quiet = [m for m in moves if not board.is_capture(m)]
-        ordered_moves = captures + quiet
-        best_move = None
+        moves.sort(key=score_move, reverse=True)
 
-        for move in ordered_moves:
+        best_move = None
+        current_flag = self.tt.UPPERBOUND
+
+        for move in moves:
             board.push(move)
-            child_score, _ = self.negamax(board, -beta, -alpha, depth-1, ply + 1)
-            score = -child_score
+            score, _ = self.negamax(board, -beta, -alpha, depth-1, ply + 1)
+            score = -score
             board.pop()
 
+            if self.stop_flag:
+                return 0, None
             if score >= beta:
+                if not board.is_capture(move):
+                    if move != self.killers[ply][0]:
+                        self.killers[ply][1] = self.killers[ply][0]
+                        self.killers[ply][0] = move
+
+                    bonus = depth * depth
+                    self.history[move.from_square][move.to_square] += bonus
+
+                self.tt.store(key, depth, beta, self.tt.LOWERBOUND, move)
                 return beta, move
+
             if score > alpha:
                 alpha = score
                 best_move = move
+                current_flag = self.tt.EXACT
 
-        if best_move is None and ordered_moves:
-              best_move = ordered_moves[0]
+        if best_move is None:
+            self.tt.store(key, depth, alpha, self.tt.UPPERBOUND, None)
+        else:
+            self.tt.store(key, depth, alpha, current_flag, best_move)
 
         return alpha, best_move
 
@@ -142,3 +220,8 @@ class Search:
             if score > alpha:
                 alpha = score
         return best_value
+
+    def check_time(self):
+        if time.time() - self.start_time > self.time_limit:
+            self.stop_flag = True
+        return self.stop_flag
